92bad01570d2950a7ede8d156fa9e23c9ad61bd0
lfortuna <lfortuna@LUISFORTUNAE647>
2017-02-04 09:43:02 -0400

    Proyect

---
+++ Recomida/Recomida.WebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Recomida.WebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Recomida/Recomida.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recomida.WebApp.MvcApplication" Language="C#" %>
